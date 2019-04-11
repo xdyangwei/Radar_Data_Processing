e1f4df73dfd46e99f5a8eabf0b889f793fdce173
xdyangwei <xdgodyw@gmail.com>
2019-04-11 20:48:37 +0800

    add the new radar data struct

--- Radar_Data.h
+++ Radar_Data.h
@@ -22,4 +22,29 @@ struct radar_data
     target_info targets[30];
     int8_t sum;
 };
+
+struct big_angle_radar_target_info{
+    float targetid;//target id
+    float x;//x
+    float y;//y
+    float v;//speed
+    float acc;//accelerate speed
+};
+
+struct big_angle_radar_data{
+    float Targets;//radar test itself flag
+    float type;//1 represents this is radar_data
+    float  version;//protocol version
+    float curtime;//time
+    big_angle_radar_target_info targets[59];
+    int compensation;//enable the struct is 1200 Byte
+};
+
+struct UDP_stream_data{
+    char a1[4];//data1 header flag
+    big_angle_radar_data b1;
+    char a2[4];//data2 header flag
+    big_angle_radar_data b2;
+};
+
 #endif //RADAR_TCP_RADAR_DATA_H
