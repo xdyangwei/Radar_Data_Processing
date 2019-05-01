c177daacf53ea62e982f1a8b5dae04949fe700fb
xdyangwei <xdgodyw@gmail.com>
2019-05-01 17:50:51 +0800

    add the system run time and date

--- sync_server.cpp
+++ sync_server.cpp
@@ -4,6 +4,9 @@
 #include "Radar_Data.h"
 #include <cstring>
 #include <memory>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/date_clock_device.hpp>
+#include <boost/progress.hpp>
 #include <boost/asio.hpp>
 using namespace boost::asio;
 namespace Sync {
@@ -24,12 +27,15 @@ namespace Sync {
     }
     void read(socket_ptr sock){
         (*sock).receive(buffer(buff));
+        std::cout<<"today:"<<boost::gregorian::day_clock::local_day()<<std::endl;
         memmove(&a,buff, sizeof(buff));
         //memset(buff,0, sizeof(buff));
         if(a.targetcount>=1&&a.header==0x55AA){
+
             for(int i=0;i<=a.targetcount-1;i++){
                 std::cout<<"x:"<<a.targets[i].x<<" y:"<<a.targets[i].y<<" distance:"<<a.targets[i].distance<<" angle:"<<a.targets[i].angle<<std::endl;
             }
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
             std::cout<<std::endl;
         }
         read(sock);
@@ -46,11 +52,13 @@ namespace Sync {
 }
 using namespace Sync;
 int main(){
+    boost::progress_timer t;
     try{
     Server s;
     s.run();
     }catch(std::exception& e){
         std::cout<<e.what()<<std::endl;
     }
+    std::cout<<t.elapsed()<<std::endl;
     return 0;
 }
